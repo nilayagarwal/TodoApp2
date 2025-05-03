4778e3f1fdc55b135398ac482145ee1784c2d26d
Nilay Agarwal <nilayagarwal@Nilays-MacBook-Air.local>
2025-05-03 15:52:13 +0530

    Update server and client integration

--- index.js
+++ index.js
@@ -3,41 +3,35 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+// Serve static files (including index.html) from current directory
+app.use(express.static(path.join(__dirname)));
+
+// Middleware
 app.use(bodyParser.json());
 
+// In-memory todos
 let todos = [];
 
+// Helper functions
 function findIndex(arr, id) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id === id) return i;
-    }
-    return -1;
+    return arr.findIndex(item => item.id === id);
 }
 
 function removeAtIndex(arr, index) {
-    let newArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (i !== index) newArray.push(arr[i]);
-    }
-    return newArray;
+    return arr.filter((_, i) => i !== index);
 }
 
-app.get('/todos', (req, res) => {
-    res.json(todos);
-});
+// Routes
+app.get('/todos', (req, res) => res.json(todos));
 
 app.get('/todos/:id', (req, res) => {
-    const todoIndex = findIndex(todos, parseInt(req.params.id));
-    if (todoIndex === -1) {
-        res.status(404).send();
-    } else {
-        res.json(todos[todoIndex]);
-    }
+    const index = findIndex(todos, parseInt(req.params.id));
+    index === -1 ? res.status(404).send() : res.json(todos[index]);
 });
 
 app.post('/todos', (req, res) => {
     const newTodo = {
-        id: Math.floor(Math.random() * 1000000), // unique random id
+        id: Math.floor(Math.random() * 1000000),
         title: req.body.title,
         description: req.body.description
     };
@@ -46,35 +40,20 @@ app.post('/todos', (req, res) => {
 });
 
 app.put('/todos/:id', (req, res) => {
-    const todoIndex = findIndex(todos, parseInt(req.params.id));
-    if (todoIndex === -1) {
-        res.status(404).send();
-    } else {
-        todos[todoIndex].title = req.body.title;
-        todos[todoIndex].description = req.body.description;
-        res.json(todos[todoIndex]);
-    }
+    const index = findIndex(todos, parseInt(req.params.id));
+    if (index === -1) return res.status(404).send();
+    todos[index] = { ...todos[index], ...req.body };
+    res.json(todos[index]);
 });
 
 app.delete('/todos/:id', (req, res) => {
-    const todoIndex = findIndex(todos, parseInt(req.params.id));
-    if (todoIndex === -1) {
-        res.status(404).send();
-    } else {
-        todos = removeAtIndex(todos, todoIndex);
-        res.status(200).send();
-    }
+    const index = findIndex(todos, parseInt(req.params.id));
+    if (index === -1) return res.status(404).send();
+    todos = removeAtIndex(todos, index);
+    res.status(200).send();
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+// Catch-all 404
+app.use((req, res) => res.status(404).send());
 
-
-// for all other routes, return 404
-app.use((req, res, next) => {
-    res.status(404).send();
-});
-
-
-app.listen(3001)
+app.listen(3001, () => console.log("Server running at http://localhost:3001"));
